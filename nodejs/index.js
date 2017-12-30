716c5fb9896a1e1b4abfa1996e2e19af9ce9dc4c
Eyad Isa <eyad.isa@icloud.com>
2017-12-30 09:06:45 -0500

    setup shell for HoneHQ chat bot - Eyad

---
+++ nodejs/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = 8000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});

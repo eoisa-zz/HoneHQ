6b6d67bcb7315e0591bbeb13609860a316b7c4a0
Eyad Isa <eyad.isa@icloud.com>
2017-12-30 16:13:33 -0500

    small refactors - Eyad

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,39 +5,40 @@ const bodyParser = require('body-parser');
 const smoochCore = require('smooch-core');
 
 const PORT = 8000;
-const KEY_ID = 'app_5a47ae74be7349003f14b076';
-const KEY_SECRET = '5zTIoGp1TQWcNiN6wgDNb0H1';
-
-const smooch = new smoochCore({
-    keyId: KEY_ID,
-    secret: KEY_SECRET,
-    scope: 'app'
-});
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/message', function (req, res) {
-    console.log('My webhook: ', JSON.stringify(req.body, null, 4));
-
     const appUserId = req.body.appUser._id;
-    console.log('app user id: ', appUserId);
-
-    if(req.body.trigger === 'conversation:start'){
-        smooch.appUsers.sendMessage(appUserId, {
-            type: 'text',
-            text: 'Hello, Hone',
-            role: 'appMaker'
-        }).then((response) => {
-            console.log('API Response: ', response);
-            res.end();
-        }).catch((err) =>{
-            console.log('API Error: ', err);
-            res.end();
-        });
+    if (req.body.trigger === 'conversation:start') {
+        process(appUserId, res)
     }
 });
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
+
+function process(userId, res) {
+    const KEY_ID = 'app_5a47ae74be7349003f14b076';
+    const KEY_SECRET = '5zTIoGp1TQWcNiN6wgDNb0H1';
+
+    const smooch = new smoochCore({
+        keyId: KEY_ID,
+        secret: KEY_SECRET,
+        scope: 'app'
+    });
+
+    smooch.appUsers.sendMessage(userId, {
+        type: 'text',
+        text: 'Hello! I\'m HoneBot! Say \'start\' to get going!',
+        role: 'appMaker'
+    }).then((response) => {
+        console.log('API Response: ', response);
+        res.end();
+    }).catch((err) => {
+        console.log('API Error: ', err);
+        res.end();
+    });
+}

a9eca2369163dddd63d66710d1005697429d350d
Eyad Isa <eyad.isa@icloud.com>
2017-12-30 10:05:52 -0500

    added webhook and fb messanger integration capture mechanism - Eyad

--- nodejs/index.js
+++ nodejs/index.js
@@ -8,6 +8,10 @@ const PORT = 8000;
 const app = express();
 app.use(bodyParser.json());
 
+app.post('/message', function (req, res) {
+    console.log('My webhook: ', JSON.stringify(req.body, null, 4))
+});
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
